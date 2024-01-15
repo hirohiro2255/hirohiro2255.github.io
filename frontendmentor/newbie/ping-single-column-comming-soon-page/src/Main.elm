fc22c959c0c6d17fb37b1bc6014db9729164421b
Hirokazu Hirono <hirono.hirokazu@gmail.com>
2024-01-16 00:57:26 +0900

    Added styling to hero section for mobile

--- frontendmentor/newbie/ping-single-column-comming-soon-page/src/Main.elm
+++ frontendmentor/newbie/ping-single-column-comming-soon-page/src/Main.elm
@@ -1,8 +1,22 @@
 module Main exposing (..)
 
-import Html exposing (Html, text)
+import Html exposing (Html, button, div, h1, h2, img, input, main_, p, section, strong, text)
+import Html.Attributes exposing (alt, class, placeholder, src, width)
 
 
 main : Html msg
 main =
-    text "Hey"
+    main_ [ class "container" ]
+        [ section [ class "hero-section" ]
+            [ div [ class "hero-wrapper" ]
+                [ h1 [ class "hero-heading" ] [ text "PING" ]
+                , h2 [ class "hero-sub-heading" ] [ text "We are launching ", strong [ class "strong-text" ] [ text "soon!" ] ]
+                , p [ class "paragraph" ] [ text "Subscribe and get notified" ]
+                , div [ class "input-group" ]
+                    [ input [ class "email", placeholder "Your email address…" ] []
+                    , button [ class "button" ] [ text "Notify Me" ]
+                    ]
+                ]
+            ]
+        , div [ class "img-section" ] [ img [ src "../src/assets/illustration-dashboard.png", alt "hero image", class "hero-img" ] [] ]
+        ]
